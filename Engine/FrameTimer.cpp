c907ffc78e3fb6aaee2fc507ccb31d8d0593a95e
Yurman <pavel.koprivec@gmail.com>
2020-09-09 20:15:26 +0200

    Add project files.

---
+++ Engine/FrameTimer.cpp
@@ -0,0 +1,15 @@
+#include "FrameTimer.h"
+
+FrameTimer::FrameTimer()
+{
+	last = std::chrono::steady_clock::now();
+}
+
+float FrameTimer::Mark()
+{
+	std::chrono::steady_clock::time_point old;
+	old = last;
+	last = std::chrono::steady_clock::now();
+	std::chrono::duration<float> time_passed = last - old;
+	return time_passed.count();
+}
